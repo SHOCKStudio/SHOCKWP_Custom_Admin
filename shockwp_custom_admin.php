9a674170ec7dc9161d9fa34359cfb741e2bc8ac0
SHOCKStudio <info@shockstudio.pl>
2018-08-18 12:43:38 +0200

    custom plugin github

--- shockwp_custom_admin.php
+++ shockwp_custom_admin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: SHOCKWP Custom Admin
-Plugin URI: https://shockstudio.pl
+Plugin URI: https://github.com/SHOCKStudio/SHOCKWP_Custom_Admin
 Description: Custom Admin Panel and login
 Version: 1.0.1
 Author: SHOCKStudio Sp. z o.o.
