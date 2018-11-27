cae8cdf8d06ae5f6f78305b32452833e2b4df6b1
SHOCKStudio <info@shockstudio.pl>
2018-11-27 11:03:29 +0100

    fix panel login, new subversion

--- shockwp_custom_admin.php
+++ shockwp_custom_admin.php
@@ -3,7 +3,7 @@
 Plugin Name: SHOCKWP Custom Admin
 Plugin URI: https://github.com/SHOCKStudio/SHOCKWP_Custom_Admin
 Description: Custom Admin Panel and login
-Version: 1.0.32
+Version: 1.0.33
 Author: SHOCKStudio Sp. z o.o.
 Author URI: https://shockstudio.pl
 GitHub Plugin URI: https://github.com/SHOCKStudio/SHOCKWP_Custom_Admin
